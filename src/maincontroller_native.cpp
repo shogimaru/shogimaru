81106b2c1c79f631359248728ae22c22909b317a
aoyama <shogimaru81@gmail.com>
2022-02-07 18:22:15 +0900

    downloading a record.

---
+++ src/maincontroller_native.cpp
@@ -0,0 +1,31 @@
+#include "maincontroller.h"
+#include "messagebox.h"
+#include "recorder.h"
+#include "sfen.h"
+#include <QFile>
+
+
+void MainController::saveFile(const QString &filePath)
+{
+    if (filePath.isEmpty()) {
+        return;
+    }
+
+    Sfen sfen(_recorder->sfenMoves(99999));
+    sfen.setPlayers(_players[maru::Sente].name(), _players[maru::Gote].name());
+
+    QFile file(filePath);
+    auto data = sfen.toCsa().toLocal8Bit();
+    if (file.open(QIODevice::WriteOnly | QIODevice::Truncate | QIODevice::Text)) {
+        int len = file.write(data);
+        file.close();
+        if (len == data.length()) {
+            // Success
+            MessageBox::information(tr("Save"), tr("Save completed."));
+            return;
+        }
+    }
+
+    // Write error
+    MessageBox::information(tr("Save Error"), tr("Failed to save the file."));
+}
