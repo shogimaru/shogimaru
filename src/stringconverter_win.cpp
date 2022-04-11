af640a1b0f9e1099bbb97fd92338bfa7124533bc
aoyama <shogimaru81@gmail.com>
2022-04-11 23:52:11 +0900

    updated

--- src/stringconverter_win.cpp
+++ src/stringconverter_win.cpp
@@ -6,14 +6,15 @@ namespace maru {
 
 QString fromShiftJis(const QByteArray &sjis)
 {
+    static auto lc = _create_locale(LC_ALL, "jpn");
     std::size_t converted;
-    std::vector<wchar_t> buf(sjis.size(), L'\0');
+    std::vector<wchar_t> buf(sjis.size() + 1, L'\0');
 
-    if (::_mbstowcs_s_l(&converted, buf.data(), buf.size(), sjis.data(), buf.size(), _create_locale(LC_ALL, "jpn")) != 0) {
+    if (::_mbstowcs_s_l(&converted, buf.data(), sjis.size(), sjis.data(), _TRUNCATE, lc) != 0) {
         return QString();
     }
+
     buf.resize(std::char_traits<wchar_t>::length(buf.data()));
-    //buf.shrink_to_fit();
     return QString::fromStdWString(std::wstring(buf.begin(), buf.end()));
 }
 
