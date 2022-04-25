a122a3e98ac777a4951dadf2a34ca7b27f86a13c
aoyama <shogimaru81@gmail.com>
2022-04-25 20:33:37 +0900

    updated

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ rem
 rem Edit this line to run the batch file for Qt environment.
 rem
 
-set SHOGIMARU_VERSION=1.2
+set SHOGIMARU_VERSION=1.3
 set BASEDIR=%~dp0
 set TARGET=shogimaru-%SHOGIMARU_VERSION%
 
